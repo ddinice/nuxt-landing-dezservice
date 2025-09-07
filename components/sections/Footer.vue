8dbd309463666560e144b100e432ceb6104426d3
Denys Daneliuk <d.danelyuk@gmail.com>
2025-09-07 11:56:12 +0200

    responsive

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="grid grid-cols-12 grid-rows-[auto_auto] md:grid-rows-[auto] gap-y-12 md:gap-y-7 gap-x-7 mt-8 md:mt-20 mb-10 p-5 md:p-15 shadow-sm border-3 rounded-xl">
-    <div class="col-span-12 md:col-span-4 row-start-2 md:row-start-1 flex flex-col gap-4">
+    <div class="col-span-12 md:col-span-5 row-start-3 md:row-start-1 flex flex-col gap-4">
       <p class="h2 font-[900]">DEZ Service.</p>
-
-      <p class="text-2xl text-gray-900">ФОП «Щербатюк»</p>
-      <p class="text-2xl text-gray-900">13601, вул. Перемоги 17, с. Ягнятин, Бердичівський р-н, Житомирська область</p>
-      
+      <p class="text-xl md:text-2xl text-gray-900">ФОП «Щербатюк»</p>
+      <p class="text-xl md:text-2xl text-gray-900">13601, вул. Перемоги 17, с. Ягнятин, Бердичівський р-н, Житомирська область</p>
     </div>
     <div class="col-span-12 md:col-span-4 row-start-1 md:row-start-1">
       <div class="flex gap-8 md:gap-16 flex-wrap md:flex-nowrap">
         <div>
           <p>Контакти</p>
-          <div class="flex flex-col gap-3 mt-3">
+          <div class="flex flex-col gap-3 mt-3 links">
             <p class="text-xl text-gray-900">+38 067 65 65 345</p>
             <p class="text-xl text-gray-900">v.shcherbatiuk@ukr.net</p>
+            <a href="/#message">
+              <p class="text-xl text-gray-900">Залишити заявку</p>
+            </a>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-span-12 md:col-span-4 row-start-1 md:row-start-1">
+    <div class="col-span-12 md:col-span-4 row-start-2 md:row-start-1">
       <div class="flex gap-8 md:gap-16 flex-wrap md:flex-nowrap">
         <div>
           <p>Навігація</p>
-          <div class="flex flex-col gap-3 mt-3">
+          <div class="flex flex-col gap-3 mt-3 links">
             <a href="/#service">
               <p class="text-xl text-gray-900">Наші Послуги</p>
             </a>
@@ -32,12 +33,24 @@
             <a href="/#faq">
               <p class="text-xl text-gray-900">FAQ</p>
             </a>
-            <a href="/#message">
-              <p class="text-xl text-gray-900">Залишити заявку</p>
-            </a>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.links p {
+  width: fit-content;
+  transition: 0.3s;
+  cursor: pointer;
+  border-bottom: 2px solid transparent;
+}
+.links p:hover {
+  border-bottom: 2px solid #ffbf30;
+}
+.links p:hover{
+  color: #000;
+}
+</style>
--- components/sections/TextBlock.vue
+++ components/sections/TextBlock.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="mt-16 md:mt-30 py-20">
+  <div class="mt-16 md:mt-30 py-10 md:py-20">
     <div>
-      <h2 class="text-center text-5xl">10+ років <span class="text-yellow-900">досвіду</span> у сфері</h2>
+      <h2 class="text-center text-3xl md:text-5xl">10+ років <span class="text-yellow-900">досвіду</span> у сфері</h2>
     </div>
     <div class="mt-8 max-w-[900px] mx-auto">
-      <p class="text-3xl text-center leading-[150%] font-[200]">
+      <p class="text-l md:text-xl lg:text-3xl text-center leading-[150%] font-[200]">
         Ми працюємо як з фізичними, так і з юридичними особами по всій території України, забезпечуючи високий рівень санітарної безпеки підприємств різних галузей
       </p>
       <br/>
-      <p class="text-3xl text-center leading-[150%] font-[200]">
+      <p class="text-l md:text-xl lg:text-3xl  text-center leading-[150%] font-[200]">
         Наша компанія спеціалізується на обслуговуванні об'єктів харчової промисловості, ресторанного бізнесу, торгових площ та виробничих приміщень. Ми розуміємо важливість дотримання міжнародних стандартів якості та безпеки, тому наша робота повністю відповідає вимогам FSSC 22000, HACCP, ISO 22000, IFS, GMP, GLP, GHP.
       </p>
       </div>
