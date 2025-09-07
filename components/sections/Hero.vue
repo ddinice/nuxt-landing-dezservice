e4bf7e6cd9c2a540f8035f9a813fccffbb68b062
Denys Daneliuk <d.danelyuk@gmail.com>
2025-09-07 20:16:51 +0200

    add word animation in hero section

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="grid grid-areas-hero grid-cols-12 grid-rows-[auto_auto_auto] md:grid-rows-[auto_auto] gap-7 mt-8 md:mt-16">
     <div class="col-span-12 row-start-1 md:col-span-6 xl:col-span-7 area-heading">
-      <h1>Захищаємо від шкідників <span class="text-yellow-900">ваш Дім та Бізнес</span>.</h1>
+      <h1 class="relative">Захищаємо від
+        <div class="relative">
+          <Transition name="word" :duration="1550">
+            <span :key="activeWord" class="block">{{ animetedWord[activeWord] }}</span>
+          </Transition>
+        </div>
+        <span class="text-yellow-900">ваш Дім та Бізнес</span>.
+      </h1>
       <p class="text-2xl whitespace-pre-line mt-4 font-[200] py-5">Дератизація, дезінсекція та дезінфекція.<br>Працюємо по всій Україні з гарантією результату.</p>
     </div>
     <div id="message" class="col-span-12 row-start-3 md:row-start-1 md:col-span-6 xl:col-span-4">
@@ -23,8 +30,49 @@
 </template>
 
 <script setup lang="ts">
+const activeWord = ref(0);
+const animetedWord = ref([
+  'шкідників',
+  'гризунів',
+  'паразитів',
+  'грибку',
+  'комах',
+  'кліщів',
+  'тарганів',
+  'клопів'
+]);
+
+let wordInterval: ReturnType<typeof setInterval> | null = null;
+
+onMounted(() => {
+  wordInterval = setInterval(() => {
+    activeWord.value = (activeWord.value + 1) % animetedWord.value.length;
+  }, 3000);
+});
+
+onBeforeUnmount(() => {
+  if (wordInterval) {
+    clearInterval(wordInterval);
+  }
+});
+
 </script>
 
 <style scoped>
+.word-enter-active,
+.word-leave-active {
+  transition: all 0.35s ease-out;
+}
 
+.word-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+  position: absolute;
+}
+
+.word-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+  position: absolute;
+}
 </style>
